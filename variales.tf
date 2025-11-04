1272f40785843f122367bbf0577ae13e898f7740
brahmaiah <bramhaiahg@gmail.com>
2025-11-04 11:00:14 +0530

    commit it

---
+++ variales.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "sg_name" {
+  type        = string
+}
+
+variable "sg_description" {
+  type        = string
+  default = ""
+}
+
+variable "vpc_id" {
+    type = string
+}
+
+variable "sg_tags" {
+    type = map
+    default = {}
+}
